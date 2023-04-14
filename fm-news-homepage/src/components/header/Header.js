0be0daeeb80d85cc2add5bfac2576e246b3e8d99
Maximum.Dev <maximum.dev@StudiosacStudio.lan>
2023-04-14 11:03:32 -0700

    created mobile menu slide out with animation

--- fm-news-homepage/src/components/header/Header.js
+++ fm-news-homepage/src/components/header/Header.js
@@ -2,72 +2,90 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-const menuStyles = `
+const menuDarkTransparentBG = `
+before:block
+before:fixed
+before:bg-black/50
+before:inset-0
+before:z-10
+`;
+
+const menuBaseClasses = `
+${/* INTERNAL LAYOUT */ ""}
 flex 
-flex-col 
-
-inset-y-0 
-
-absolute 
-bg-color-almost-white
+flex-col
 pt-36
 px-6
 space-y-6
 w-9/12
-translate-x-full`;
 
-const openMenuStyles = `
-translate-0
+${/* POSITION ON PAGE*/ ""}
+inset-y-0
+right-0 
+fixed
+z-20
+
+${/* COLORS */ ""}
+bg-color-almost-white
+
+${/* ANIMATION */ ""}
+transition-transform
 `;
 
-const headerPsudoElement = `
-before:block
-before:absolute
-before:bg-black/50
-before:inset-0
+const openMenuClasses = `
+transform-x-0
+${menuDarkTransparentBG}
+`;
+
+const closeMenuClasses = `
+  transform-x-full
 `;
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header
-      className={`flex justify-between pt-7 pb-8 ${
-        isOpen ? headerPsudoElement : ""
-      }`}
-    >
-      <Image src="/images/logo.svg" width="64" height="40" />
-      <button
-        className="absolute top-0 right-0 z-10"
-        onClick={() => setIsOpen((isOpen) => !isOpen)}
+    <>
+      <header
+        className={`flex justify-between fixed top-0 inset-x-0 pt-7 pb-8`}
       >
-        {isOpen ? (
-          <div>
-            <Image src="/icons/menu-close.svg" width="30" height="30" />
-          </div>
-        ) : (
-          <div>
-            <Image src="/icons/menu-open.svg" width="40" height="17" />
-          </div>
-        )}
-      </button>
-      <ul className={`${menuStyles} ${isOpen ? `${openMenuStyles}` : ""}`}>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/new">New</Link>
-        </li>
-        <li>
-          <Link href="/popular">Popular</Link>
-        </li>
-        <li>
-          <Link href="/trending">Trending</Link>
-        </li>
-        <li>
-          <Link href="/categories">Categories</Link>
-        </li>
-      </ul>
-    </header>
+        <Image src="/images/logo.svg" width="64" height="40" />
+        <button
+          className="absolute top-0 right-0 z-30"
+          onClick={() => setIsOpen((isOpen) => !isOpen)}
+        >
+          {isOpen ? (
+            <div>
+              <Image src="/icons/menu-close.svg" width="30" height="30" />
+            </div>
+          ) : (
+            <div>
+              <Image src="/icons/menu-open.svg" width="40" height="17" />
+            </div>
+          )}
+        </button>
+        <ul
+          className={`${menuBaseClasses} ${
+            isOpen ? `${openMenuClasses}` : `${closeMenuClasses}`
+          }`}
+        >
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/new">New</Link>
+          </li>
+          <li>
+            <Link href="/popular">Popular</Link>
+          </li>
+          <li>
+            <Link href="/trending">Trending</Link>
+          </li>
+          <li>
+            <Link href="/categories">Categories</Link>
+          </li>
+        </ul>
+      </header>
+    </>
   );
 }
 
